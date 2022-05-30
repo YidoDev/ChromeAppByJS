aecb1b86c3bf72ddd3e6a7654113d1f9db1c08c8
HC <yido0426@gmail.com>
2022-05-30 23:13:28 +0900

    studying on 05.30.

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-function plus(a, b) {
-    console.log(a + b);
-};
+const age = parseInt(prompt("How old are you?"));
 
-plus(5, 10);
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real positive number.");
+}
+else if(age >= 0 && age <= 18) {
+    console.log("You are too young.");
+}
+else if(age > 18 && age < 50) {
+    console.log("You can drink.");
+}
+else if(age >= 50 && age < 80) {
+    console.log("You can drink. But be careful.")
+}
+else if(age === 100) {
+    console.log("Wow. You are wise.");
+}
+else if(age >= 80) {
+    console.log("You can't drink.");
+}
